9562ffdc7b290fc09f717509a9160e67badd8c86
lllinh <030239230109@st.buh.edu.vn>
2026-04-08 15:22:30 +0700

    bai

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,11 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\MovieController;
 
 class MovieController extends Controller
 {
     //
-    public function index(){
-        return view("movie.index");
-    }
+    public function index()
+{
+    $genres = DB::table('genre')->get();
+    $movies = DB::table('movie')
+        ->where('popularity', '>', 450)
+        ->where('vote_average', '>', 7)
+        ->orderBy('release_date', 'desc')
+        ->limit(12)
+        ->get();
+
+    return view('movie.index', compact('genres', 'movies'));
+}
+public function trang_the_loai($id)
+{
+
+    $genres = DB::table('genre')->get();
+
+
+    $movies = DB::table('movie')
+        ->join('movie_genre', 'movie.id', '=', 'movie_genre.id_movie')
+        ->where('movie_genre.id_genre', $id) // Lưu ý dấu gạch dưới nếu DB của bạn có nó
+        ->select('movie.*')
+        ->distinct()
+        ->orderBy('movie.release_date', 'desc')
+        ->limit(12)
+        ->get();
+
+
+    $genreName = DB::table('genre')->where('id', $id)->value('genre_name_vn');
+
+    return view('movie.index', compact('genres', 'movies', 'genreName'));
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OpenRouterController;
+use App\Http\Controllers\MovieController;
 
 Route::get('/', [App\Http\Controllers\MovieController::class, 'index']);
 
 
 
 Route::get('/openrouter', [OpenRouterController::class, 'chat']);
+
+Route::get('/theloai/{id}', [MovieController::class, 'trang_the_loai'])->name('genre');
+
